2c81fc20f619f4ba3b48a285370ca06a375fe452
jakehume <jakehume@umich.edu>
2022-03-06 17:50:58 -0500

    more setup.py experimentation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='EarReaderSSD',
+    version='1.0',
+    packages=['EarReaderSSD'],
+    url='',
+    license='',
+    author='jacobhume',
+    author_email='jakehume@umich.edu',
+    install_requires=['numpy', 'pyfiglet', 'pandas', 'nltk', 'mido', 'music21', 'pyo'],
+    description=''
+)
