4e24ab81cb0d634d42547631710f12f1e0971fac
jakehume <jakehume@umich.edu>
2022-03-06 21:31:38 -0500

    more setup.py experimentation

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='EarReaderSSD',
     version='1.0',
-    packages=[],
+    packages=find_packages(),
     url='',
     license='',
     author='jacobhume',
