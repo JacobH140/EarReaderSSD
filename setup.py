bc95bf6ad796afddcec8b5390c5e5284d724076f
jakehume <jakehume@umich.edu>
2022-03-06 21:37:03 -0500

    more setup.py experimentation

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     license='',
     author='jacobhume',
     author_email='jakehume@umich.edu',
+    py_modules=['playback', 'sonify_character', 'sonify_sentence', 'sonify_text', 'sonify_word', 'syntax_tree_v2'],
     install_requires=['numpy', 'pyfiglet', 'pandas', 'nltk', 'mido', 'music21', 'pyo'],
     description=''
 )
